51b4cc222889057f8f9fbf42cbaf1f2172e8ef82
Alan Asmis <asmisalan@gmail.com>
2024-10-17 18:25:02 -0300

    chore: update the config to include missing images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/<repository-name>" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/<repository-name>" : "",
 };
 
 export default nextConfig;
